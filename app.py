0a9c2159f79b64aa15b043415099593be8ac01cc
23 <kameshwaran@nextbraintech.com>
2022-07-18 17:31:17 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,227 @@
+import imghdr
+import os
+from flask import Flask, render_template, request, redirect, url_for, abort, \
+    send_from_directory
+from werkzeug.utils import secure_filename
+from PIL import Image
+import base64
+import io
+
+app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+app.config['UPLOAD_PATH'] = 'uploads'
+
+def validate_image(stream):
+    header = stream.read(512)  # 512 bytes should be enough for a header check
+    stream.seek(0)  # reset stream pointer
+    format = imghdr.what(None, header)
+    if not format:
+        return None
+    return '.' + (format if format != 'jpeg' else 'jpg')
+
+@app.route('/')
+def index():
+    files = os.listdir(app.config['UPLOAD_PATH'])
+    return render_template('index.html', files=files)
+
+@app.route('/', methods=['POST'])
+def upload_files():
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
+                file_ext != validate_image(uploaded_file.stream):
+            abort(400)
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+    return redirect(url_for('model'))
+
+@app.route('/uploads/<filename>')
+def upload(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
+
+@app.route('/result', methods = ['GET','POST'])
+def model():
+    from scipy.spatial import distance as dist
+    from imutils import perspective
+    import numpy as np
+    import imutils
+    import cv2
+
+
+    # Class for point
+    class point:
+        x = 0
+        y = 0
+
+    # Points for Left Right Top and Bottom
+    plL = point()
+    plR = point()
+    plU = point()
+    plD = point()
+
+    def midpoint(ptA, ptB):
+        return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+
+    DirPath = '/home/developer/python /Projects/FISH BAY/uploads'
+    ext = ['png', 'jpg', 'gif']  # Add image formats here
+    Files = os.listdir(DirPath)
+    for File in Files:
+        img_path = os.path.join(DirPath, File)
+
+
+    image = cv2.imread(img_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+    # perform edge detection, then perform a dilation + erosion to
+    # close gaps in between object edges
+    edged = cv2.Canny(gray, 50, 100)
+    edged = cv2.dilate(edged, None, iterations=1)
+    edged = cv2.erode(edged, None, iterations=1)
+
+    # For finding midpoint
+    def midpoint(ptA, ptB):
+        return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+
+    # find contours in the edge map
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    cnts = cnts[0]
+
+    # sort the contours from left-to-right and, then initialize the
+    # distance colors and reference object
+    # (cnts, _) = contours.sort_contours(cnts)
+    colors = ((0, 0, 255), (240, 0, 159), (0, 165, 255), (255, 255, 0),
+              (255, 0, 255))
+    refObj = None
+    pixelsPerMetric = None
+
+    # loop over the contours individually
+    for c in cnts:
+        # if the contour is not sufficiently large, ignore it
+        if cv2.contourArea(c) < 500:
+            continue
+
+        # compute the rotated bounding box of the contour
+        box = cv2.minAreaRect(c)
+        box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+        box = np.array(box, dtype="int")
+
+        # order the points in the contour such that they appear
+        # in top-left, top-right, bottom-right, and bottom-left
+        # order, then draw the outline of the rotated bounding
+        # box
+        box = perspective.order_points(box)
+
+        # compute the center of the bounding box
+        cX = np.average(box[:, 0])
+        cY = np.average(box[:, 1])
+
+        # if reference object is not set then we presume
+        # the origin i.e. the (0,0) of the camera frame
+        # as reference point
+        if refObj is None:
+            box1 = np.zeros(box.shape)
+            rcX = 0
+            rcY = 0
+            refObj = (box1, (rcX, rcY), 27.6)
+
+        # draw the contours on the image
+        orig = image.copy()
+        cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+        cv2.drawContours(orig, [refObj[0].astype("int")], -1, (0, 255, 0), 2)
+
+        # stack the reference coordinates and the object coordinates
+        # to include the object center
+
+        refCoords = np.vstack([refObj[0], refObj[1]])
+        objCoords = np.vstack([box, (cX, cY)])
+
+        # Give them values
+        plL.x = box[0][0]
+        plL.y = box[0][1]
+
+        plR.x = box[1][0]
+        plR.y = box[1][1]
+
+        plU.x = box[2][0]
+        plU.y = box[2][1]
+
+        plD.x = box[3][0]
+        plD.y = box[3][1]
+
+        # Finding Height and width
+        for (x, y) in box:
+            cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+
+        # unpack the ordered bounding box, then compute the midpoint
+        # between the top-left and top-right coordinates, followed by
+        # the midpoint between bottom-left and bottom-right coordinates
+        (tl, tr, br, bl) = box
+        (tltrX, tltrY) = midpoint(tl, tr)
+        (blbrX, blbrY) = midpoint(bl, br)
+
+        # compute the midpoint between the top-left and top-right points,
+        # followed by the midpoint between the top-righ and bottom-right
+        (tlblX, tlblY) = midpoint(tl, bl)
+        (trbrX, trbrY) = midpoint(tr, br)
+
+        # draw the midpoints on the image
+        cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+        cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+        cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+        cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+
+        # draw lines between the midpoints
+        cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
+                 (255, 0, 255), 2)
+        cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
+                 (255, 0, 255), 2)
+
+        # compute the Euclidean distance between the midpoints
+        dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+        dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+
+        # if the pixels per metric has not been initialized, then
+        # compute it as the ratio of pixels to supplied metric
+        # (in this case, inches)
+        if pixelsPerMetric is None:
+            pixelsPerMetric = 27.6  # This ratio needs to be set manually
+
+        # compute the size of the object
+        dimA = dA / pixelsPerMetric
+        dimB = dB / pixelsPerMetric
+
+        # draw the object sizes on the image
+        cv2.putText(orig, "{:.1f}in".format(dimA),
+                    (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.65, (0, 0, 0), 2)
+        cv2.putText(orig, "{:.1f}in".format(dimB),
+                    (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.65, (0, 0, 0), 2)
+
+        cv2.imwrite('/home/developer/python /Projects/FISH BAY/static/result/image.jpg', orig)
+
+        # Full script.
+        im = Image.open("/home/developer/python /Projects/FISH BAY/static/result/image.jpg")
+        data = io.BytesIO()
+        im.save(data, "JPEG")
+        encoded_img_data = base64.b64encode(data.getvalue())
+
+
+        # for removing the uploaded image in uploads folderS
+        absolute_path = os.path.join(os.getcwd(), 'uploads')
+        for file in os.listdir(absolute_path):
+            if file.endswith('.jpg'):
+                os.remove(os.path.join(absolute_path, file))
+
+
+    return render_template('result.html', img_data=encoded_img_data.decode('utf-8'))
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
